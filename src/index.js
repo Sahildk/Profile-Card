439e4a52ce298e1baeb90d1ddbc0f4e7e155fc99
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-07-13 04:18:13 +0530

    first deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="/src/sahil.jpg" alt="Sahil Deore" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Sahil Deore</h1>
+      <p>
+        A web developer passionate about creating sleek, user-friendly websites
+        that make an impact. With expertise in [key skills, e.g., JavaScript,
+        React, etc.], I love turning ideas into engaging digital experiences.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="❤️" color="#009dd1" />
+      <Skill skill="JavaScript" emoji="💥" color="#e5de00" />
+      <Skill skill="HTML+CSS" emoji="🔥" color="Orange" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
