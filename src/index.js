4e11c4b13cfd728dc28fe183b716a1a480cc2c0b
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-07-16 02:09:39 +0530

    finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
@@ -18,7 +50,13 @@ function App() {
 }
 
 function Avatar() {
-  return <img className="avatar" src="/src/sahil.jpg" alt="Sahil Deore" />;
+  return (
+    <img
+      className="avatar"
+      src={`${process.env.PUBLIC_URL}/sahil.jpg`}
+      alt="Sahil Deore"
+    />
+  );
 }
 function Intro() {
   return (
@@ -35,25 +73,26 @@ function Intro() {
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="❤️" color="#009dd1" />
-      <Skill skill="JavaScript" emoji="💥" color="#e5de00" />
-      <Skill skill="HTML+CSS" emoji="🔥" color="Orange" />
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 }
-function Skill(props) {
+
+function Skill({ skill, color, level }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
